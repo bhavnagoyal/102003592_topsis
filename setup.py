29d620efea9ba2051862c6a9611af265c2d8dae2
bhavnagoyal <95182361+bhavnagoyal@users.noreply.github.com>
2023-01-18 17:33:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,29 @@
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Topsis",
+    version="1.0.4",
+    author="Bhavna Goyal",
+    author_email="bgoyal_be20@thapar.edu",
+    description="A package -> Calculates Topsis Score and Rank them accordingly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/Tewatia5355/Topsis_tewatia",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["102003592_topsis"],
+    include_package_data=True,
+    install_requires='pandas',
+    entry_points={
+        "console_scripts": [
+            "topsis=102003592_topsis.topsis:main",
+        ]
+    },
+)
