fb67a70d79fd3a0754ad2fcea016cd473484a3aa
bhavnagoyal <95182361+bhavnagoyal@users.noreply.github.com>
2023-01-21 07:43:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
-    url="https://github.com/Tewatia5355/Topsis_tewatia",
+    url="https://github.com/bhavnagoyal/102003592_topsis",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
